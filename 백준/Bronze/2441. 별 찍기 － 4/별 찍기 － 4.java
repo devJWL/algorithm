d656c0a13b592e300240cd7bf9cb912efa89b90c
devJWL <88548682+devJWL@users.noreply.github.com>
2024-01-18 18:41:51 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 128 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+    public static void main(String[] args) throws IOException {
+
+
+        int n = Integer.valueOf(br.readLine());
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < n; ++i) {
+            sb.append('*');
+        }
+
+        for (int i = 0; i < n; ++i) {
+            bw.write(sb.toString() + "\n");
+            sb.setCharAt(i, ' ');
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
