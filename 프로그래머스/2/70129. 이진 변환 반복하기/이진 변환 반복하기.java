e31cbfd13dc87fa9f4800d89f9aaeba18086ed72
devJWL <88548682+devJWL@users.noreply.github.com>
2024-01-25 18:38:07 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 19.45 ms, Memory: 90.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+
+
+class Solution {
+    public String deleteZero(String s) {
+        StringBuffer sb = new StringBuffer();
+        char[] charArr = s.toCharArray();
+        for (char ch : charArr){
+            if (ch == '1') sb.append(ch);
+        }
+        return sb.toString();
+    }
+    
+    public String toBinary(int number) {
+        StringBuffer sb = new StringBuffer();
+        while(number > 0) {
+            sb.append(number % 2);
+            number /= 2;
+        }
+        return sb.reverse().toString();
+    }
+    
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        while(!s.equals("1")) {
+            ++answer[0];
+            answer[1] += s.length();
+            s = deleteZero(s);
+            answer[1] -= s.length();
+            s = toBinary(s.length());
+        }
+        return answer;
+    }
+}
