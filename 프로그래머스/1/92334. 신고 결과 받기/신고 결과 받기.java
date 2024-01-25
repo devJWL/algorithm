21eccbfdf9d4ceaa69b6a85436d46842332f2c09
devJWL <88548682+devJWL@users.noreply.github.com>
2024-01-25 15:53:00 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.17 ms, Memory: 68.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        
+        Map<String, Set<String>> reportList = new HashMap<>();
+        Map<String, Integer> reportedCountMap = new HashMap<>();
+        
+        for (String id : id_list) {
+            reportList.put(id, new HashSet<>());
+            reportedCountMap.put(id, 0);
+        }
+        
+        for (String repo : report) {
+            String[] token = repo.split(" ");
+            String reporter = token[0];
+            String reportee = token[1];
+            
+            if (!reportList.get(reporter).contains(reportee)){
+                reportList.get(reporter).add(reportee);
+                reportedCountMap.put(reportee, reportedCountMap.get(reportee) + 1);
+            }
+        }
+        
+        for (int i = 0; i < id_list.length; ++i) {
+            int cnt = 0;
+            Set<String> set = reportList.get(id_list[i]);
+            for (String id : set) {
+                if (reportedCountMap.get(id) >= k) {
+                    ++cnt;
+                }
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+}
