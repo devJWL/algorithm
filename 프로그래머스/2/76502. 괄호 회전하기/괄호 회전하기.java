ec154bea35ba6322f9df6135e21390f910cb087b
devJWL <88548682+devJWL@users.noreply.github.com>
2024-02-01 09:14:01 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.22 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,54 +2,50 @@ import java.util.*;
 
 
 class Solution {
-	private boolean isVaild(String s, int offset) {
-		int len = s.length();
-		Stack<Character> st = new Stack<>();
-		int i = offset;
-		do {
-			
-			char ch = s.charAt(i);
-			
-			if (ch == '(' || ch == '{' || ch == '[') {
-				st.push(ch);
-			}
-			else {
-				if (st.isEmpty()) {
-					return false;
-				}
-				if (ch == ')') {
-					if (st.pop() != '(') {
-						return false;
-					}
-				}
-				else if (ch == '}') {
-					if (st.pop() != '{') {
-						return false;
-					}
-				}
-				else {
-					if (st.pop() != '[') {
-						return false;
-					}
-				}
-			}
-			i = (i + 1) % len;
-		}while(i != offset);
-		
-		if (st.isEmpty()) {
-			return true;
-		}
-		return false;
-	}
-	
-	
-	public int solution(String s) {
-		int answer = 0;
-		for (int i = 0; i < s.length(); ++i) {
-			if (isVaild(s, i)) {
-				++answer;
-			}
-		}
-		return answer;
-	}
+    private boolean isVaild(String s, int offset) {
+        int len = s.length();
+        Stack<Character> st = new Stack<>();
+        int i = offset;
+        do {
+
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                if (ch == ')') {
+                    if (st.pop() != '(') {
+                        return false;
+                    }
+                } else if (ch == '}') {
+                    if (st.pop() != '{') {
+                        return false;
+                    }
+                } else {
+                    if (st.pop() != '[') {
+                        return false;
+                    }
+                }
+            }
+            i = (i + 1) % len;
+        }while(i != offset);
+
+        if (st.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    public int solution(String s) {
+        int answer = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (isVaild(s, i)) {
+                ++answer;
+            }
+        }
+        return answer;
+    }
 }
