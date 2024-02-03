b18279ac94acdfa451206afb3bd55e0240b54de6
devJWL <88548682+devJWL@users.noreply.github.com>
2024-02-03 17:09:11 +0900

    [Silver I] Title: 미로 탐색, Time: 596 ms, Memory: 295812 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,73 +1,51 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+import java.io.*;
 
 
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int[] nm = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int n = nm[0];
+        int m = nm[1];
+        char[][] map = new char[n][m];
 
-    static int n, m;
-    static char[][] map;
-    static int[][] dist;
-    static int[] dy = {1, -1, 0, 0};
-    static int[] dx = {0, 0, -1, 1};
-
-    static class Point {
-        int y, x;
-        Point(int y, int x){
-            this.y = y;
-            this.x = x;
-        }
-    }
-
-    public static void main() throws IOException {
-
-        String[] token = br.readLine().split(" ");
-        n = Integer.valueOf(token[0]);
-        m = Integer.valueOf(token[1]);
-        map = new char[n][m];
-        dist = new int[n][m];
-
-        for (int i = 0; i < n; ++i){
+        for (int i = 0; i < n; ++i) {
             map[i] = br.readLine().toCharArray();
         }
 
-        Queue<Point> q = new LinkedList<Point>();
-        q.add(new Point(0, 0));
+        int[][] dist = new int[n][m];
+        int[] dy = {-1, 1, 0, 0};
+        int[] dx = {0, 0, -1, 1};
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0});
         dist[0][0] = 1;
-        while(!q.isEmpty()){
-            Point curr = q.poll();
-            if (curr.y == n - 1 && curr.x == m - 1) break;
 
-            for (int dir = 0; dir < 4; ++dir){
-                int ny = curr.y + dy[dir];
-                int nx = curr.x + dx[dir];
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
+            int y = cur[0];
+            int x = cur[1];
+            if (y == n - 1 && x == m - 1) break;
+
+            for (int dir = 0; dir < 4; ++dir) {
+                int ny = y + dy[dir];
+                int nx = x + dx[dir];
                 if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
                 if (map[ny][nx] == '0') continue;
-                if (dist[ny][nx] != 0) continue;
-                if (dist[ny][nx] >= dist[curr.y][curr.x] + 1) continue;
-                q.add(new Point(ny, nx));
-                dist[ny][nx] = dist[curr.y][curr.x] + 1;
+                if (dist[ny][nx] >= dist[y][x] + 1) continue;
+                q.add(new int[]{ny, nx});
+                dist[ny][nx] = dist[y][x] + 1;
             }
         }
 
-        bw.write(dist[n - 1][m - 1] + "");
+        bw.write(dist[n - 1][m - 1] + "\n");
 
-        br.close();
         bw.flush();
         bw.close();
+        br.close();
     }
-
-
-    public static void main(String[] args) throws IOException {
-        Main.main();        
-    }    
 }
