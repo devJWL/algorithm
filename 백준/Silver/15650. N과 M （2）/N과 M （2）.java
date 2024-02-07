40810c6b730b7a6d86f37598e9e9a0f98c1ccda1
Juno <88548682+devJWL@users.noreply.github.com>
2024-02-08 00:17:37 +0900

    [Silver III] Title: N과 M (2), Time: 148 ms, Memory: 16140 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+
+
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m;
+    static int[] nums = new int[10];
+    static boolean[] used = new boolean[10];
+
+    static void BT(int curr, int k) throws IOException {
+        if (k == m) {
+            for (int i = 0; i < m; ++i){
+                bw.write(nums[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        if (curr == n + 1) return;
+
+        nums[k] = curr;
+        BT(curr + 1, k + 1);
+        BT(curr + 1, k);
+    }
+    public static void main(String[] args) throws IOException {
+
+
+
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        n = input[0];
+        m = input[1];
+
+        BT(1, 0);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
