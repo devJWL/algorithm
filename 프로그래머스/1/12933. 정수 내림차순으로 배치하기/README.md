608ac4d19eb909ce8457aa680abe85220df82f22
Juno <88548682+devJWL@users.noreply.github.com>
2024-03-07 09:28:36 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=java) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.01 ms
+메모리: 72.5 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 07일 09:26:56
+2024년 03월 07일 09:28:35
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public static long solution(long n) {
+        long answer = 0;
+        int[] cnt = new int[10];
+        
+        while (n != 0) {
+            cnt[(int)(n % 10)]++;
+            n /= 10;
+        }
+
+        for (int i = 9; i >= 0; i--) {
+            while (cnt[i] != 0) {
+                answer = answer * 10 + i;
+                cnt[i]--;
+            }
+        }
+        
+        return answer;
+    }
+}
