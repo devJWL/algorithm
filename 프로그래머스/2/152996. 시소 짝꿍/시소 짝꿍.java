e54b65133edf3630abb79eacde25661aadb20d28
Juno <88548682+devJWL@users.noreply.github.com>
2024-03-14 09:52:17 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.11 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int[] weights) {
+        
+        Map<Integer, Long> cnt = new HashMap();
+        for (int weight : weights){
+            cnt.put(weight, cnt.getOrDefault(weight, 0L)+1);
+        }
+
+        long answer = 0;
+        for (int weight : cnt.keySet()){
+            long n = cnt.get(weight);
+            if (n > 1) answer += (n - 1) * n / 2;
+            
+            if (weight % 2 == 0) answer += n * cnt.getOrDefault(weight / 2 * 3, 0L);
+            if (weight % 3 == 0) answer += n * cnt.getOrDefault(weight / 3 * 4, 0L);
+            answer += n * cnt.getOrDefault(weight * 2, 0L);
+        }
+        return answer;
+    }
+}
