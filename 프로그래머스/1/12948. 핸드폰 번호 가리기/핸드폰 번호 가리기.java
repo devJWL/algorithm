cf1a5468f2d56e713912648599ecb169c8c0650d
Juno <88548682+devJWL@users.noreply.github.com>
2024-03-15 09:20:26 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,12 +1,9 @@
-import java.util.Arrays;
-
 class Solution {
-    public String solution(String phone_number) {
-        char[] stars = new char[phone_number.length() - 4];
-        Arrays.fill(stars, '*');
-        String answer = new String(String.valueOf(stars));
-        String lastFourNumbers = phone_number.substring(phone_number.length() - 4);
-        answer += lastFourNumbers;
-        return answer;
-    }
+  public String solution(String phone_number) {
+     char[] ch = phone_number.toCharArray();
+     for(int i = 0; i < ch.length - 4; i ++){
+         ch[i] = '*';
+     }
+     return String.valueOf(ch);
+  }
 }
