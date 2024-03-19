f2f495ad9e2a00a0b854bffdb4036bc61603fd91
Juno <88548682+devJWL@users.noreply.github.com>
2024-03-19 09:23:29 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,21 +1,23 @@
 class Solution {
-    boolean[] used;
+    boolean[] visited;
+    int[][] dun;
     int answer = 0;
     
-    void BT(int k, int[][] dungeons, int cnt) {
-        for (int i = 0; i < dungeons.length; ++i) {
-            if (used[i]) continue;
-            if (dungeons[i][0] > k) continue;
-            used[i] = true;
-            BT(k - dungeons[i][1], dungeons, cnt + 1);
-            used[i] = false;
+    void BT(int k, int cnt) {
+        for (int i = 0; i < dun.length; ++i) {
+            if (visited[i]) continue;
+            if (dun[i][0] > k) continue;
+            visited[i] = true;
+            BT(k - dun[i][1], cnt + 1);
+            visited[i] = false;
         }
         answer = Math.max(answer, cnt);
     }
     
     public int solution(int k, int[][] dungeons) {
-        used = new boolean[dungeons.length];
-        BT(k, dungeons, 0);
+        visited = new boolean[dungeons.length];
+        dun = dungeons;
+        BT(k, 0);
         return answer;
     }
 }
