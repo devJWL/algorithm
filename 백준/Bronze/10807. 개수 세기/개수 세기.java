a6793f600ace97b56dceb360b481bd44f59120b4
Juno <88548682+devJWL@users.noreply.github.com>
2025-01-13 22:25:47 +0900

    [Bronze V] Title: 개수 세기, Time: 128 ms, Memory: 16236 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,24 +1,21 @@
 import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 public class Main {
-  static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int v = Integer.parseInt(br.readLine());
 
-  public static void main(String[] args) throws IOException {
+        long count = Arrays.stream(nums).filter(num -> num == v).count();
 
-    int n = Integer.parseInt(br.readLine());
-    List<Integer> arr = Arrays.stream(br.readLine().split(" "))
-        .map(Integer::parseInt)
-        .collect(Collectors.toList());
-    
-    int v = Integer.parseInt(br.readLine());
+        bw.write(count + "\n");
 
-    bw.write(arr.stream().filter(e -> e == v).count() + " ");
-    br.close();
-    bw.flush();
-    bw.close();
-  }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
